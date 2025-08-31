dab648d10b562bddc7873e8501732982b0aefc22
Lucca <claudia@coffee-code.tech>
2025-08-31 16:35:28 -0300

    feat(card): adjusted list card

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import CardHeader from "./components/CardHeader"
 import CardItems from "./components/CradItems"
 import Header from "./components/Header"
 
-
 function App() {
-	
-
+	const cardItemsLenght = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 	return (
 		<div className="w-full flex items-center flex-col justify-center">
 			<Header />
 			<CardHeader />
-			<CardItems />
+			<div className="grid grid-cols-2 gap-4 p-4">
+				{cardItemsLenght.map((item) => (
+					<CardItems key={item} />
+				))}
+			</div>
 		</div>
 	)
 }
